4ee056fc6b74875f2864a84b7d47e407151a9552
markbneal <mark.neal@dairynz.co.nz>
2020-09-15 14:53:46 +1200

    now includes district level (in addition to previous region level)

--- pasture yield.R
+++ pasture yield.R
@@ -15,7 +15,8 @@ head(db)
 # i.e. Season 2012-13 makes year_ending 2013
 db <- db %>% 
   as_tibble() %>% 
-  mutate(year_ending = as.numeric(str_sub(Season,1,4))+1)
+  mutate(year_ending = as.numeric(str_sub(Season,1,4))+1) #%>% 
+  #filter( year_ending != 2019)# filtering out last year of data sends Canterbury trend to less significance.
 
 #filter out NA for pasture harvest and Map region number to names
 db <- db %>% 
@@ -31,6 +32,8 @@ db <- db %>%
                             Region == 8 ~ "Otago-Southland"
                             ))
 
+# Join district data
+
 
 #This data structure is just right for plotting crop yield over time!
 
@@ -44,7 +47,7 @@ db %>%
 
 ggsave("pasture by year, facet by region.png", width = 8, height=6) #saves last plot
 
-#### Many models ####
+#### Many models #### Region Level
 # Now let’s fit a linear model to each country-crop combination.
 library(tidymodels)
 
@@ -93,4 +96,66 @@ slopes_db %>%
 
 ggsave("linear trend and p value, pasture by year, facet by region.png", width = 8, height=6) #saves last plot
 
+#### Now repeat for district level ####
 
+# read and join district data for district names
+
+library(readxl)
+District_key <- read_xlsx("List of TLAs against Dairy Statistics and Economic Survey Regions v3 MN.xlsx")
+
+colnames(db)
+unique(db$District.council)
+colnames(District_key)
+unique(District_key$DairyBase_District_Number)
+
+db <- left_join(db, District_key, by = c("District.council" = "DairyBase_District_Number"))
+
+
+#### Many models #### District Level
+# Now let’s fit a linear model to each country-crop combination.
+library(tidymodels)
+
+class(db)
+District_key$DistrictDairyStatistics_2018
+tidy_lm_db <- db %>%
+  select(c("DistrictDairyStatistics_2018", "year_ending", "Pasture.and.Crop.harvested.t.DM.ha")) %>%  #select only essential columns. Data to nest within region, because multiple observations for each region at each year
+  nest(yields = c(year_ending, Pasture.and.Crop.harvested.t.DM.ha)) %>%
+  mutate(model = map(yields, ~ lm(Pasture.and.Crop.harvested.t.DM.ha ~ year_ending, data = .x)))
+
+#rlang::last_error()
+tidy_lm_db
+
+
+# Next, let’s tidy() those models to get out the coefficients, 
+# and adjust the p-values for multiple comparisons while we’re at it.
+
+slopes_db <- tidy_lm_db %>%
+  mutate(coefs = map(model, tidy)) %>%
+  unnest(coefs) %>%
+  filter(term == "year_ending") %>%
+  mutate(p.value = p.adjust(p.value))
+
+slopes_db
+
+
+#### Explore results ####
+# Now we can visualize the results of this modeling, 
+# which is estimating how crop yields are changing around the world.
+
+library(ggrepel)
+slopes_db %>%
+  ggplot(aes(estimate, p.value, label = DistrictDairyStatistics_2018, colour = DistrictDairyStatistics_2018 )) +
+  geom_vline(
+    xintercept = 0, lty = 2,
+    size = 1.5, alpha = 0.7, color = "gray50"
+  ) +
+  geom_point( alpha = 0.8, size = 2.5, show.legend = FALSE) +
+  scale_y_log10() +
+  #facet_wrap(~Region) +
+  geom_text_repel(size = 3) +
+  theme_light() +
+  theme(strip.text = element_text(size = 12)) +
+  labs(x = "increase in t DM per hectare per year")+
+  theme(legend.position = "none")
+
+ggsave("linear trend and p value, pasture by year, facet by district.png", width = 8, height=6) #saves last plot
